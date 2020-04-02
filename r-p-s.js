56bbc6a89739004a3e347b7f74babd44ccd4bcab
Lucia Brammer <brammerl96@gmail.com>
2020-04-01 17:16:45 -0700

    final code working

--- r-p-s.js
+++ r-p-s.js
@@ -1,10 +1,13 @@
 import getRandomThrow from './getRandomThrow.js';
+import checkResults from './checkResult.js';
 
 const myButton = document.getElementById('fight');
 const winSpan = document.getElementById('wins');
-const winLoseSpan = document.getElementById('winLose');
+const winLose = document.getElementById('message');
 const lossSpan = document.getElementById('losses');
 const drawSpan = document.getElementById('draws');
+const makeVisible = document.getElementById('roundResult');
+const updatedImage = document.getElementById('compImage');
 
 let winTally = 0;
 let lossTally = 0;
@@ -16,11 +19,31 @@ const updateMatchStats = () => {
     drawSpan.textContent = drawTally;
 };
 
+myButton.addEventListener('click', () => {
+    const compChoice = getRandomThrow();
+    const playerRadio = document.querySelector('input:checked');
+    const playerChoice = playerRadio.value;
 
+    const results = checkResults(playerChoice, compChoice);
+    
+    if (results === 'draw') {
+        drawTally++;
+        updateMatchStats();
+        makeVisible.style.visibility = 'visible';
+        updatedImage.src = 'https://chrysalis.com.au/images/Headlam-Stalemate-2709.jpg';
+        winLose.textContent = `IT'S A DRAW! Your oponent chose: ${compChoice}`;
+    } else if (results === 'win') {
+        winTally++;
+        makeVisible.style.visibility = 'visible';
+        updatedImage.src = 'https://www.uxmetric.com/wp-content/uploads/2018/05/quickwin-uxmetric.png';
+        winLose.textContent = `YOU WIN! Your oponent chose: ${compChoice}`;
+        updateMatchStats();
+    } else if (results === 'lose') {
+        lossTally++;
+        updateMatchStats();
+        makeVisible.style.visibility = 'visible';
+        updatedImage.src = 'https://image.shutterstock.com/image-vector/you-lose-red-rubber-stamp-260nw-244769227.jpg';
+        winLose.textContent = `YOU LOSE! Your oponent chose: ${compChoice}`;
+    }
 
-
-
-
-
-
-
+});
